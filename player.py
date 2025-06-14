3e2f9ad5a37ab88eebe133adc2193f2d7e570d7f
Malderok <cardaju@gmail.com>
2025-06-14 09:47:00 +0200

    La nave se mueve

--- player.py
+++ player.py
@@ -27,7 +27,11 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    
+    def move(self, dt):
+
+        distance = pygame.Vector2(0,1).rotate(self.rotation)
+
+        self.position += distance * PLAYER_SPEED * dt
 
 
     def update(self, dt):
@@ -37,3 +41,7 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)  

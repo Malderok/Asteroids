e33803d6470d0c6999d3ff4679a4aa508a86cb6c
Malderok <cardaju@gmail.com>
2025-06-14 09:15:08 +0200

    Ya tenemos jugador y puede rotar!

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
 
 	negro = pygame.Color(0,0,0)
 
+
 	pygame.init()
+	reloj = pygame.time.Clock()
+	dt = 0.0
+	nave = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(negro)
+
+		nave.update(dt)
+		nave.draw(screen)
+
 		pygame.display.flip()
+
+		#Guardamos la cantidad de tiempo que ha pasado desde el ultimo frame en segundos(milisegundos / 1000)
+		dt = reloj.tick(60)/1000.0
             
 
 
